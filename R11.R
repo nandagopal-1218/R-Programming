90ca8440b5ef03bbe6149c6784dd9a1a59cf3a9a
nandagopal-1218 <86040622+nandagopal-1218@users.noreply.github.com>
2021-06-17 12:42:10 +0530

    Add files via upload

---
+++ R11.R
@@ -0,0 +1,20 @@
+#Data Structures in R
+
+#----Vectors----
+# A vector is a one-dimensional array. 
+# We can create a vector with all the basic data type
+# The simplest way to build a vector is to use the c command.
+# Creating a Numerical Vector:
+vec_num <- c(1, 10, 49)
+vec_num
+class(vec_num)
+# Creating a Character Vector:
+vec_chr <- c("a", "b", "c")
+vec_chr
+class(vec_chr)
+# Creating a Boolean Vector
+vec_bool <-  c(TRUE, FALSE, TRUE)
+vec_bool
+class(vec_bool)
+
+#----Matrix----
